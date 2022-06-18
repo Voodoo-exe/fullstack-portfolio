9a65b4c6061a21da4737c4f63f38dc0f58279b35
Voodoo-exe <58384762+Voodoo-exe@users.noreply.github.com>
2022-06-18 12:04:56 +0530

    navbar and images

--- portfolio_frontend/src/App.js
+++ portfolio_frontend/src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import { About, Footer, Header, Skills, Testimonials, Work } from './containers';
+import './App.scss'
 
 const App = () => {
     return (
       <div className="app">
+        {/* <Navbar /> */}
         <Header />
         <About />
         <Work />
-        <Skills/>
+        <Skills />
         <Testimonials />
         <Footer />
-        <Navbar />
       </div>
     );
 }
